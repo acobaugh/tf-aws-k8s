677de0fa68b0e2c50045f2c5cd449d87ba9a2204
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-03-12 11:13:17 -0400

    add some outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,23 @@
+output "user-kubeconfig" {
+  value = "${module.bootkube.user-kubeconfig}"
+}
 
+output "cluster_name" {}
+output "cluster_fqdn" {}
+output "api_hostname" {}
+
+output "master_iam_role_arn" {
+  value = "${aws_iam_role.master_role.arn}"
+}
+
+output "master_sg_id" {
+  value = "${aws_security_group.master.id}"
+}
+
+output "worker_iam_role_arn" {
+  value = "${aws_iam_role.worker_role.arn}"
+}
+
+output "worker_sg_id" {
+  value = "${aws_security_group.worker.id}"
+}
